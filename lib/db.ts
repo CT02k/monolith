da981940ca4ac7e1a2aa1ed933ee886b0cb90fd9
CT02k <joaopaulocfwork@gmail.com>
2026-04-22 20:20:32 -0300

    created db save playlist util

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,36 @@
 import { youtube_v3 } from "@googleapis/youtube/build/v3"
+import { prisma } from "./prisma"
 
-export async function savePlaylisConversion(convertedTracks: {
+export async function savePlaylistConversion({
+  spotifyUrl,
+  spotifyId,
+  convertedTracks,
+  url,
+}: {
+  spotifyUrl: string
+  spotifyId: string
   convertedTracks: (youtube_v3.Schema$SearchResult | undefined)[]
   url: string
 }) {
-  const res = await prisa
+  return await prisma.playlist.create({
+    data: {
+      spotifyUrl,
+      spotifyId,
+      youtubeUrl: url,
+      tracks: {
+        create: convertedTracks
+          .filter((track): track is youtube_v3.Schema$SearchResult => !!track)
+          .map((track) => ({
+            title: track.snippet?.title || "Unknown Title",
+            youtubeId: track.id?.videoId || "Unknown ID",
+            channelName: track.snippet?.channelTitle || "Unknown Channel",
+            thumbnailUrl:
+              track.snippet?.thumbnails?.high?.url ||
+              track.snippet?.thumbnails?.medium?.url ||
+              track.snippet?.thumbnails?.default?.url ||
+              "",
+          })),
+      },
+    },
+  })
 }
